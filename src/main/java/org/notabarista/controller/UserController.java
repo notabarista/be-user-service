a1ca4f5d68fcec31b9dcea7fd2edb30ffac8ad5c
Jozsef Sas <jozsef.l.sas@gmail.com>
2021-03-09 18:37:59 +0200

    new test endpoint to get logged in user details

--- src/main/java/org/notabarista/controller/UserController.java
+++ src/main/java/org/notabarista/controller/UserController.java
@@ -1,18 +1,20 @@
 package org.notabarista.controller;
 
-import org.apache.http.HttpStatus;
 import org.notabarista.controller.abstr.AbstractDeleteController;
 import org.notabarista.dto.UserDTO;
 import org.notabarista.entity.UserEntity;
 import org.notabarista.exception.AbstractNotabaristaException;
 import org.notabarista.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
@@ -27,7 +29,18 @@ public class UserController extends AbstractDeleteController<UserEntity, UserDTO
 
         // TODO is this the best way to redirect from service URL to gateway URL?
         httpServletResponse.setHeader("Location", "/");
-        httpServletResponse.setStatus(HttpStatus.SC_MOVED_TEMPORARILY);
+        httpServletResponse.setStatus(HttpStatus.MOVED_PERMANENTLY.value());
+    }
+
+    @GetMapping("/details")
+    public ResponseEntity<UserDTO> getUserDetails(@RequestHeader("uid") String userId) {
+        Optional<UserDTO> userDTOOptional = userService.findByUserIdentifier(userId);
+
+        if (userDTOOptional.isPresent()) {
+            return new ResponseEntity<>(userDTOOptional.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
 }

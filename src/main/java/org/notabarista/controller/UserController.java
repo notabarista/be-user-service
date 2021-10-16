a352d86d41304e68c614793dca30a73d188ceff1
Jozsef Sas <jozsef.l.sas@gmail.com>
2021-10-16 18:11:16 +0300

    new endpoint for getting user by identifier
    This is different from getting by PostgreSQL's auto generated id.

--- src/main/java/org/notabarista/controller/UserController.java
+++ src/main/java/org/notabarista/controller/UserController.java
@@ -1,8 +1,11 @@
 package org.notabarista.controller;
 
+import org.apache.commons.lang3.time.StopWatch;
 import org.notabarista.controller.abstr.AbstractDeleteController;
 import org.notabarista.dto.UserDTO;
 import org.notabarista.entity.UserEntity;
+import org.notabarista.entity.response.Response;
+import org.notabarista.entity.response.ResponseStatus;
 import org.notabarista.exception.AbstractNotabaristaException;
 import org.notabarista.service.IUserService;
 import org.notabarista.util.NABConstants;
@@ -10,11 +13,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -38,6 +44,23 @@ public class UserController extends AbstractDeleteController<UserEntity, UserDTO
 
         if (userDTOOptional.isPresent()) {
             return new ResponseEntity<>(userDTOOptional.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/identifier/{identifier}")
+    public ResponseEntity<Response<UserDTO>> getUserByIdentifier(@PathVariable String identifier) {
+        StopWatch watch = new StopWatch();
+        watch.start();
+        Optional<UserDTO> userDTOOptional = userService.findByUserIdentifier(identifier);
+        List<UserDTO> dtos = userDTOOptional.isPresent() ? List.of(userDTOOptional.get()) : new ArrayList<>();
+        watch.stop();
+
+        if (userDTOOptional.isPresent()) {
+            return new ResponseEntity<>(
+                    new Response<>(ResponseStatus.SUCCESS, watch.getTime(), dtos, dtos.size(), 1, 0, dtos.size(), ""),
+                    HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
